7557d9764ea728dfeda60f5382c88bebd626c185
user <user@DESKTOP-N5K2OO2>
2023-10-17 10:02:08 -0500

    Integración de buenas practicas c# con patrones de diseño

---
+++ PruebaTecnica/Models/Pokemon.cs
@@ -0,0 +1,32 @@
+﻿namespace PruebaTecnica.Models
+{
+    public class Pokemon
+    {
+        public string Name;
+        public int Height;
+        public int Weight;
+        public int Attack;
+        public int HP;
+        public int Defense;
+        public int Speed;
+    }
+
+    public class PokemonTypeResultModel
+    {
+        public List<PokemonTypePokemonModel> Pokemon { get; set; }
+    }
+    public class PokemonTypePokemonModel
+    {
+        public PokemonTypePokemonItemModel Pokemon { get; set; }
+    }
+    public class PokemonTypePokemonItemModel
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+    }
+
+    public class PokemonParams { 
+        public string valor1 { get; set; }
+        public string valor2 { get; set; }
+    }
+}
---
+++ PruebaTecnica/Services/PokemonService.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+using PruebaTecnica.Models;
+using System.Security.Cryptography;
+
+namespace PruebaTecnica.Services
+{
+    public class PokemonService
+    {
+        private readonly HttpClient _httpClient;
+
+        public PokemonService(HttpClient httpClient){
+            this._httpClient = httpClient;
+        }
+
+        public List<Pokemon> GetPokemons(List<PokemonParams> pokemonParams) { 
+            var lstPokemon = new List<Pokemon>();
+            string baseUrl = "https://pokeapi.co/api/v2/";
+            /*Temporal, borrar. Cambiar por params*/
+            List<string> typePokemons = new List<string> {
+                "Fire", "Electric"
+            };
+            try
+            {
+                foreach (var pokemon in pokemonParams) {
+                    string urlType = $"{baseUrl}type/{pokemon}/";
+                    HttpResponseMessage response = _httpClient.GetAsync(urlType).Result;
+                    response.EnsureSuccessStatusCode();/*Agregar if*/
+                    string responseContent = response.Content.ReadAsStringAsync().Result;
+                    var jsonPokemon = JsonConvert.DeserializeObject<PokemonTypeResultModel>(responseContent);
+                    foreach (var item in jsonPokemon.Pokemon){
+                        string itemUri = item.Pokemon.Url;
+                        HttpResponseMessage responseMessage = _httpClient.GetAsync(itemUri).Result;
+                        responseMessage.EnsureSuccessStatusCode();
+                        string respopnseContent = responseMessage.Content.ReadAsStringAsync().Result;
+                        var pokemonValue = JsonConvert.DeserializeObject<Pokemon>(respopnseContent);
+                        lstPokemon.Add(pokemonValue);
+                    }
+                }
+
+                return lstPokemon;
+            }
+            catch (Exception ex)
+            {
+                return new List<Pokemon>();
+            }
+        }
+    }
+}
